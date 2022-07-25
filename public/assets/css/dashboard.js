6bfdd1be8150fd422030910b5830ff91771d76b6
ajaymaurya <ajay.maurya90@gmail.com>
2022-07-26 00:16:23 +0200

    webcore package setup and orm installation

---
+++ public/assets/css/dashboard.js
@@ -0,0 +1,21 @@
+"use strict";
+(self["webpackChunkgbc"] = self["webpackChunkgbc"] || []).push([["css/dashboard"],{
+
+/***/ "./assets/css/dashboard.css":
+/*!**********************************!*\
+  !*** ./assets/css/dashboard.css ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/css/dashboard.css"));
+/******/ }
+]);
